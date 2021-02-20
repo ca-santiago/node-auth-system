554da35c8cb45ead4c4f0eaf2af78b42401df79a
Carmen Santiago <greminoficial@gmail.com>
2021-02-19 23:18:23 -0600

    [Add] Dockerfiles

--- src/shared/core/index.ts
+++
@@ -1,9 +0,0 @@
-
-import config from '../../config';
-import { LocalFileRepo } from './LocalFileRepo';
-
-const fileRepo = new LocalFileRepo(config.baseDir, config.baseHost, config.fileStorageName);
-
-export {
-    fileRepo,
-}
