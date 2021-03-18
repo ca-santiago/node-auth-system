ace48794c9f191fc4ce058df57f2ea8d42fa4f5e
Santiago <greminoficial@gmail.com>
2021-03-18 15:38:38 -0600

    [Mod] Services disable by default. Mongo connection to singe URL

--- src/app/StartServices.ts
+++ src/app/StartServices.ts
@@ -1,25 +1,25 @@
-import {StartAuthSubscriptions} from "../modules/auth/subscriptions";
-import { initMongoConnection, MongoConnectionConf } from "../shared/infra/mongodb";
-import {GetMQInstance} from "../shared/services/MQ";
-import { loadMongoConfig } from "./loadMongoConfig";
+// import { StartAuthSubscriptions } from "../modules/auth/subscriptions";
+// import { GetMQInstance } from "../shared/services/MQ";
+import { initMongoConnection } from "../shared/infra/mongodb";
 
 export async function StartServices() {
-  await LoadMogoService();
-	try {
-	 await GetMQInstance();
-   await StartAuthSubscriptions();
-	} catch (err) {}
+  try {
+    // await LoadMogoService();
+    // await GetMQInstance();
+    // await StartAuthSubscriptions();
+  } catch (err) {
+    process.exit();
+  }
 }
-
 
 async function LoadMogoService() {
   try {
-    console.log('[DB] Starting database conection');
-    const mongoConfig: MongoConnectionConf = loadMongoConfig();
-    await initMongoConnection(mongoConfig);
-    console.log('[DB] MongoDB connection started');
+    console.log("[DB] Starting database conection");
+    const conUrl = process.env.MONGO_URL || "mongo://localhost:27017";
+    await initMongoConnection(conUrl);
+    console.log("[DB] MongoDB connection started");
   } catch (err) {
-    console.log('[DB] Could not connect to database');
-    console.error(`[DB] ${err.message}`)
+    console.log("[DB] Could not connect to database");
+    console.error(`[DB] ${err.message}`);
   }
 }
